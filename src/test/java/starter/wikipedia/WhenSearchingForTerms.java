a39005ae511e731cc5f8999053334a60522ce628
Martin Blazek <martinblazek6@gmail.com>
2023-05-28 14:23:30 +0200

    spravil som login test

--- src/test/java/starter/wikipedia/WhenSearchingForTerms.java
+++ src/test/java/starter/wikipedia/WhenSearchingForTerms.java
@@ -1,13 +1,14 @@
 package starter.wikipedia;
 
 import net.serenitybdd.core.Serenity;
-import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.SingleBrowser;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,7 +29,7 @@ class WhenSearchingForTerms {
     /**
      * Actions related to searches. This is a UIInteraction class so it will be instantiated automatically by Serenity.
      */
-    SearchActions search;
+    SearchActions actions;
 
     /**
      * A page object representing a Wikipedia article that is currently appearing in the browser.
@@ -36,12 +37,16 @@ class WhenSearchingForTerms {
      */
     DisplayedArticle displayedArticle;
 
+    private final String expectedUrlAfterLogin = "https://www.saucedemo.com/inventory.html";
+
     @Test
-    void searchBySingleKeyword() {
+    void loginPageValid() {
         navigate.toTheHomePage();
-        search.searchBy("Everest");
-        Serenity.reportThat("The first heading should be 'Mount Everest'",
-                () -> assertThat(displayedArticle.getFirstHeading()).isEqualTo("Mount Everest")
-        );
+//        actions.sendKeysUsername("standard_user");
+        actions.sendKeysUsername("locked_out_user");
+        actions.sendKeysPassword("secret_sauce");
+        actions.clickLoginButton();
+
+        Assertions.assertEquals(expectedUrlAfterLogin,driver.getCurrentUrl());
     }
 }
